51eaf60d8720809653b750b9f750d5745e1a98c8
Carmen Alvarez <carmen@rmen.ca>
2024-07-13 23:02:13 +0200

    Support oauth token refresh.

--- walkingpadfitbit/auth/client.py
+++ walkingpadfitbit/auth/client.py
@@ -1,4 +1,7 @@
+from typing import Any
+
 from authlib.integrations.httpx_client import AsyncOAuth2Client
+from authlib.oauth2.auth import OAuth2Token
 
 from walkingpadfitbit.auth import storage
 
@@ -12,8 +15,16 @@ def get_client(
     if not token:
         return None
 
+    async def update_token(token: dict[str, Any], **kwargs):
+        print("update_token")
+        if token.get("success") is False:
+            raise Exception("bad refresh token")  # TODO specific exception
+        storage.save_oauth_token(OAuth2Token.from_dict(token))
+
     return AsyncOAuth2Client(
         client_id,
         client_secret,
         token=token,
+        token_endpoint="https://api.fitbit.com/oauth2/token",
+        update_token=update_token,
     )
