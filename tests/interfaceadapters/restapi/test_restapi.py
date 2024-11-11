45d362ca301ad8afb243a707abddee858f829be6
Carmen Alvarez <carmen@rmen.ca>
2024-11-11 01:58:23 +0100

    Add test scenarios for the toggle-start-stop route.

--- tests/interfaceadapters/restapi/test_restapi.py
+++ tests/interfaceadapters/restapi/test_restapi.py
@@ -8,6 +8,10 @@ from pytest import MonkeyPatch
 from werkzeug.test import TestResponse
 
 from tests.fakes.ph4_walkingpad.config import configure_fake_walkingpad
+from tests.fakes.ph4_walkingpad.fakecontroller import (
+    ControllerScenario,
+    FakeWalkingPadCurStatus,
+)
 from tests.fakes.ph4_walkingpad.fakescanner import ScannerScenario
 from walkingpadfitbit import container
 
@@ -18,6 +22,7 @@ class Scenario:
     route: str
     expected_status_code: int
     expected_body: dict[str, Any] | None = None
+    controller_scenario: ControllerScenario | None = None
 
 
 SCENARIOS = [
@@ -30,6 +35,26 @@ SCENARIOS = [
         id="stop",
         route="stop",
         expected_status_code=http.HTTPStatus.NO_CONTENT,
+    ),
+    Scenario(
+        id="toggle started",
+        route="toggle-start-stop",
+        expected_status_code=http.HTTPStatus.OK,
+        expected_body={"status": "started"},
+    ),
+    Scenario(
+        id="toggle stopped",
+        route="toggle-start-stop",
+        expected_status_code=http.HTTPStatus.OK,
+        expected_body={"status": "stopped"},
+        controller_scenario=ControllerScenario(
+            last_status=FakeWalkingPadCurStatus(
+                dist=10,
+                time=5,
+                belt_state=1,
+                speed=1,
+            )
+        ),
     ),
 ]
 
@@ -50,6 +75,7 @@ def test_treadmill_command(
             ScannerScenario(
                 found_addresses=["some address"],
             ),
+            controller_scenario=scenario.controller_scenario,
         )
 
         container.config.set("device.name", "some device")
